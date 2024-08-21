78fb37dd2577a6e31cddcd814a304c68b58173ad
Matteus Moreno <matteus_moreno@live.com>
2024-08-21 08:48:55 -0300

    initial commit

---
+++ src/main/java/com/matteusmoreno/mail_sender_moto_manager/MailSenderMotoManagerApplication.java
@@ -0,0 +1,13 @@
+package com.matteusmoreno.mail_sender_moto_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailSenderMotoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailSenderMotoManagerApplication.class, args);
+	}
+
+}
